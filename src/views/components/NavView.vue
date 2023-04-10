17b1f9365596e812c2c62fa7934d75eb555b8485
soybean15 <marlonpadilla1593@gmail.com>
2023-04-10 12:23:54 +0800

    nav in progress

--- src/views/components/NavView.vue
+++ src/views/components/NavView.vue
@@ -1,9 +1,15 @@
 <template>
-  <div class="flex flex-col h-full md:flex-row bg-red-500">
+  <div class="flex flex-col nav md:flex-row bg-green-400 overflow-visible">
     <div class="p-2 m-2">
-      <img class="w-24 h-24 object-cover rounded-full" src="@/assets/img/vote-logo.jpeg"/>
+      <img class="w-24 border border-green-400 h-24 object-cover rounded-full" src="@/assets/img/vote-logo.jpeg"/>
     </div>
-    <div class="flex-grow">items</div>
+    <div class="flex-grow flex items-center">
+      <div v-for="navItem in navItems" :key="navItem.name">
+        <router-link :to="{name:navItem.route_name}">
+          <div class="h-24 flex items-end md:pl-12 text-xl hover:text-2xl text-gray-100 semi-bold">{{ navItem.name }}</div>
+        </router-link>
+      </div>
+    </div>
     <div>signin</div>
   </div>
 </template>
@@ -13,10 +19,21 @@ import { useAuthStore } from "@/stores/auth";
 export default {
   setup() {
     const authStore = useAuthStore();
-    return { authStore };
+
+
+    const navItems = [
+      {route_name:'home', name:'Home',route:'/'},
+      {route_name:'about',name:'Admin',route:'/'},
+      {route_name:'home',name:'Results',route:'/'},
+
+    ]
+    return { authStore,navItems };
   },
 };
 </script>
 
 <style>
+.nav{
+  height: 5.6rem;
+}
 </style>
