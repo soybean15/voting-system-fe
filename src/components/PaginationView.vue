84aee415ce561277d86ae40236ab64ffe75e6524
soybean15 <marlonpadilla1593@gmail.com>
2023-04-22 10:56:23 +0800

    done pagination basic functionality

--- src/components/PaginationView.vue
+++ src/components/PaginationView.vue
@@ -1,99 +1,105 @@
 <template>
-  <div class="flex">
-    <button @click="handlePrevious" class="page-button text-xs">Previous</button>
+  <div class="flex" v-if="currentPage">
+    <button @click="handlePrevious" class="page-button text-xs">
+      Previous
+    </button>
     <div v-for="page in showedPage" :key="page">
-        <button @click="handleClickPage(page)" class="page-button text-xs"  :class="{'page-active': currentPage==page}"   >{{ page }}</button>
+      <button
+        @click="handleClickPage(page)"
+        class="page-button text-xs"
+        :class="{ 'page-active': currentPage == page }"
+      >
+        {{ page }}
+      </button>
     </div>
-    <button  @click="handleNext" class="page-button text-xs">Next</button>
+    <button @click="handleNext" class="page-button text-xs">Next</button>
   </div>
   {{ currentPage }}
   {{ pages }}
 </template>
 
 <script>
-import { computed, ref } from 'vue';
+import { computed, ref } from "vue";
 export default {
   props: ["totalPages"],
-  emits: ['onClickPage','onNext','onPrevious'],
-  setup(props,{emit}) {
+  emits: ["onClickPage", "onNext", "onPrevious"],
+  setup(props, { emit }) {
+    let totalPages = props.totalPages;
+    let currentPage = ref();
+    let perPage = 5;
+    let first = ref(0);
+    let last = ref(first.value + perPage);
 
-    let totalPages = props.totalPages
-    let currentPage = ref()
-    let perPage  =5
-
-    if(totalPages>0){
-        currentPage.value=1
+    if (totalPages > 0) {
+      currentPage.value = 1;
     }
 
     const pages = ref([]);
-    const createArrayFromOneToN =(n)=> {
-      
+    const createArrayFromOneToN = (n) => {
       for (let i = 1; i <= n; i++) {
         pages.value.push(i);
       }
-    }
+    };
 
+    createArrayFromOneToN(totalPages);
 
-    createArrayFromOneToN(totalPages)
+    const handleClickPage = (page) => {
+      emit("onClickPage");
+      currentPage.value = page;
+    };
 
+    const handleNext = () => {
+      if (currentPage.value < totalPages) {
+        currentPage.value++;
 
-   
-
-    const handleClickPage=(page)=>{
-        emit('onClickPage')
-        currentPage.value =page
-    }
-
-    const handleNext=()=>{
-        if(currentPage.value<totalPages){
-             currentPage.value++
+        if (currentPage.value > last.value) {
+          first.value += perPage;
+          last.value = first.value + perPage;
         }
-       
-    }
-    const handlePrevious=() =>{
-        if(currentPage.value>1){
-            currentPage.value--
+      }
+    };
+    const handlePrevious = () => {
+      if (currentPage.value > 1) {
+        currentPage.value--;
+
+        if (currentPage.value <= first.value) {
+          first.value -= perPage;
+          last.value = first.value + perPage;
         }
-        
-    }
+      }
+    };
 
-   
-    const showedPage= computed(()=>{
+    const showedPage = computed(() => {
+      const start = first.value;
+      const end = start + perPage;
+      console.log("start " + start);
+      const pageToShow = pages.value.slice(start, end);
 
-        const start = (currentPage.value - 1) * perPage;
-        const end = start + perPage;
-        const pageToShow = pages.value.slice(start, end);
-        return pageToShow;
-    })
+      return pageToShow;
+    });
 
     return {
-        showedPage,
-        handleClickPage,
-        currentPage,
-        handleNext,
-        handlePrevious,
-        pages
-    }
-
+      showedPage,
+      handleClickPage,
+      currentPage,
+      handleNext,
+      handlePrevious,
+      pages,
+    };
   },
 };
 </script>
 
 <style scoped>
-.page-button{
-    background-color: rgba(0, 0, 0, 0.041);
-    padding: .5em;
-    margin :2px;
-    color:rgb(119, 119, 119);
-    border:1px solid;
-    border-radius: 5px;
-    
+.page-button {
+  background-color: rgba(0, 0, 0, 0.041);
+  padding: 0.5em;
+  margin: 2px;
+  color: rgb(119, 119, 119);
+  border: 1px solid;
+  border-radius: 5px;
 }
-.page-active{   
-    background-color: rgba(54, 54, 54, 0.281);
-   
+.page-active {
+  background-color: rgba(54, 54, 54, 0.281);
 }
-
-
-
-</style>
+</style> 
