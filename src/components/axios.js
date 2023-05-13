ba3c9da77c88f146cecda6b39f10961155bce0db
soybean15 <marlonpadilla1593@gmail.com>
2023-05-13 23:23:40 +0800

    hello

--- src/components/axios.js
+++ src/components/axios.js
@@ -2,4 +2,4 @@ import axios from 'axios'
 
 axios.defaults.withCredentials = true
 //axios.defaults.baseURL = "http://localhost:8000"
-//axios.defaults.baseURL = "http://localhost:8000"
+//axios.defaults.baseURL = "http://pollingpoint.net/"
--- src/data/fetchData.js
+++ src/data/fetchData.js
@@ -2,7 +2,8 @@ import {ref} from 'vue'
 
 const fetchData = async (route) => {
     const error = ref(null);
-    const api =  "http://localhost:8000/"
+    //const api =  "http://localhost:8000/"
+    const api = "http://pollingpoint.net/"
 
     try {
       let data = await fetch(api+route);
