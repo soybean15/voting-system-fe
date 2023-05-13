1a0273184ce780fca2672ddd2afbf1816ddefb61
soybean15 <marlonpadilla1593@gmail.com>
2023-05-13 23:03:03 +0800

    rec

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,16 +2,22 @@
   <div class="home w-5/6 h-full m-auto">
 
 
+    <div class="relative ">
+      
+      <!-- <div class=" absolute left-50 bg-white w-40 h-40"></div> -->
+      <div class="home-content h-full flex flex-col justify-center items-center mt-24">
+     
+     <div class="font-extrabold text-8xl text-white text-slate-50 underline decoration-sky-500">Polling-Point.com</div>
+     <div class=" text-xl text-gray-300 mt-2 font-mono">Introducing our new voting web app – designed to make your voting experience more convenient and accurate than ever before. Say goodbye to long queues and confusing ballot papers – our intuitive interface and secure voting system ensure a seamless and stress-free voting process.</div>
+     <div class="flex">
+       <button @click="authStore.handleStart" class="mt-4 btn btn-blue drop-shadow-lg">Get Started</button>
+  
+     </div>
+   </div>
 
-    <div class="home-content h-full flex flex-col justify-center items-center">
-      <div class="font-extrabold text-8xl text-white text-slate-50 underline decoration-sky-500">Polling-Point.com</div>
-      <div class=" text-xl text-gray-300 mt-2 font-mono">Introducing our new voting web app – designed to make your voting experience more convenient and accurate than ever before. Say goodbye to long queues and confusing ballot papers – our intuitive interface and secure voting system ensure a seamless and stress-free voting process.</div>
-      <div class="flex">
-        <button @click="authStore.handleStart" class="mt-4 btn btn-blue drop-shadow-lg">Get Started</button>
-   
-      </div>
     </div>
 
+   
 
     
     <!-- <StatusView/> -->
