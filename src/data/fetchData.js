d5e2357da2818a724a3f4e0461cf3b8f7ae72fcb
soybean15 <marlonpadilla1593@gmail.com>
2023-05-14 18:15:31 +0800

    adad

--- src/data/fetchData.js
+++ src/data/fetchData.js
@@ -3,10 +3,10 @@ import {ref} from 'vue'
 const fetchData = async (route) => {
     const error = ref(null);
     //const api =  "http://localhost:8000/"
-    const api = "http://api.pollingpoint.net/"
+    const api = "https://api.pollingpoint.net/"
 
     try {
-      let data = await fetch(api+route);
+      let data = await fetch(api+route);n
       if (!data.ok) {
         throw Error("no Data available");
       }
